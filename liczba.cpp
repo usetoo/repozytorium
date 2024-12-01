31f415ce3c7bae2c969f073d52f5d4c6ca34b91a
Justyna Szczypka <usetysia@o2.pl>
2024-12-01 01:44:20 +0100

    Dodano walidację wczytanych liczb

--- liczba.cpp
+++ liczba.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
+
+bool czy_doskonala(int liczba) {
+    int suma = 0;
+    for (int i = 1; i < liczba; ++i) {
+        if (liczba % i == 0) suma += i;
+    }
+    return suma == liczba;
+}
+
 
 int main() {
     int liczba;
     cin >> liczba;
     cout << "Wczytano: " << liczba << endl;
+    if (liczba <= 0) {
+        cerr << "Podano liczbę niedodatnią!" << endl;
+        return 1;
+    }
+    cout << "Wczytano: " << liczba;
+    if (czy_doskonala(liczba)) {
+        cout << " - liczba doskonala" << endl;
+    } else {
+        cout << " - nie jest liczba doskonala" << endl;
+    }
     return 0;
 }
 
