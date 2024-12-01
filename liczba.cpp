1320cd157ecb1a03020361e3f00ef010c49b2f3a
Justyna Szczypka <usetysia@o2.pl>
2024-12-01 01:40:22 +0100

    Dodano funkcję czy_doskonala

--- liczba.cpp
+++ liczba.cpp
@@ -2,10 +2,23 @@
 
 using namespace std;
 
+bool czy_doskonala(int liczba) {
+    int suma = 0;
+    for (int i = 1; i < liczba; ++i) {
+        if (liczba % i == 0) suma += i;
+    }
+    return suma == liczba;
+}
+
 int main() {
     int liczba;
     cin >> liczba;
     cout << "Wczytano: " << liczba << endl;
+     if (czy_doskonala(liczba)) {
+        cout << " - liczba doskonala" << endl;
+    } else {
+        cout << " - nie jest liczba doskonala" << endl;
+    }
     return 0;
 }
 
